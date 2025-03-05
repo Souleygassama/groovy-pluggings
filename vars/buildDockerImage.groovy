5d62fe340d632837880e659a055bd38f03c3f636
Souleygassama <souleygassama@gmail.com>
2025-03-06 00:29:07 +0100

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,7 +1,11 @@
 def call (String imageName, String dockerFilePath){
     echo "building Docker image ${imageName} from /var/lib/jenkins/workspace/sama-gp/${dockerFilePath}"
     docker.build (imageName, "--file "+dockerFilePath+" .")
-    sh "docker tag ${imageName}:latest souleygassama/${imageName}"
-
-    docker.image("souleygassama/"+imageName).push()
+    docker.withRegistry('https://index.docker.io/v1/', 'docker-hub-credentials') {
+    def dockerUser = "souleygassama"
+    def imageName = "sama-gp-annonce-image"
+    def fullImageName = "${dockerUser}/${imageName}:latest"
+        
+    sh "docker tag ${imageName}:latest ${fullImageName}"
+    docker.image(fullImageName).push()
 }
