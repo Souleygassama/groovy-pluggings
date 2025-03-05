dacec388f3a38418a245a66987350955878f4624
Souleygassama <souleygassama@gmail.com>
2025-03-05 11:36:12 +0100

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,4 +1,4 @@
-def buildDockerImage(String imageName, String dockerFilePath){
+def call (String imageName, String dockerFilePath){
     echo "building Docker image ${imageName} from ${dockerFilePath}"
     docker.build (imageName, dockerFilePath)
     docker.image(imageName).push()
