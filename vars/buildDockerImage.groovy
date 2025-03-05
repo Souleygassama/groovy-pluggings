fd2e6d777c671afd992bcf10b277dc3bdbf3b1e3
Souleygassama <souleygassama@gmail.com>
2025-03-06 00:18:47 +0100

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 def call (String imageName, String dockerFilePath){
     echo "building Docker image ${imageName} from /var/lib/jenkins/workspace/sama-gp/${dockerFilePath}"
     docker.build (imageName, "--file "+dockerFilePath+" .")
-    docker.image(imageName).push()
+    docker.image("souleygassama/"+imageName).push()
 }
