281dbd06fe47c8816e831c842c5ed799351b0522
Souleygassama <souleygassama@gmail.com>
2025-03-06 21:22:37 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -1,17 +1,14 @@
 def call() {
     echo "Copying jar to workers"
- sshagent(['vargrant-public-key']) {
+sshagent(['vargrant-public-key']) {
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp"
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
-
-        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 << 'EOF'
-            # Stop any running instance of the application
+        scp -o StrictHostKeyChecking=no target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
+        
+        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "
             pkill -f 'sama-gp-annonce.jar' || true
-            
-            # Start the application in the background
             nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &
-        EOF
+        "
     """
 }
        
