46236575c5f3d8d850ae9b0080768f4ececf7c5d
Souleygassama <souleygassama@gmail.com>
2025-03-06 12:27:47 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -1,7 +1,8 @@
 def call() {
     echo "Copying jar to workers"
     sh """
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/opt/deployment/sama-gp/sama-gp-annonce.jar
+        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deployment/sama-gp"
+        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deployment/sama-gp/sama-gp-annonce.jar
         ssh vagrant@192.168.56.11 << 'EOF'
             pkill -f 'sama-gp-annonce.jar' || true  # Stop existing process if running
             nohup java -jar /opt/deployment/sama-gp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
