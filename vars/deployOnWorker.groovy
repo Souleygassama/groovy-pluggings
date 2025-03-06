202bf99782a36e426b38bd0d9de645165d7af0a6
Souleygassama <souleygassama@gmail.com>
2025-03-06 22:20:44 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -3,7 +3,7 @@ def call() {
 sshagent(['vargrant-public-key']) {
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp"
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar 
+        rsync -e "ssh -o StrictHostKeyChecking=no" target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &"
     """
 }
