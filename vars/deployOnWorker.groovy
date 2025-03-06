11b04e4afe8d05a66ba1e6fd0436e5406337e881
Souleygassama <souleygassama@gmail.com>
2025-03-06 21:07:23 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -20,6 +20,9 @@ def call() {
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp"
         scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
+         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 << 'EOF'
+                nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &
+            EOF
     """
 }
     /*
