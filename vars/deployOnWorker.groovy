0877b15d05d5fce35e22d57a2ce4573e0dbd9241
Souleygassama <souleygassama@gmail.com>
2025-03-06 20:03:08 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -18,11 +18,11 @@ def call() {
     
     sshagent(['vargrant-public-key']) {
     sh """
-        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /opt/deployment/sama-gp/sama-gp6"
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/opt/deployment/sama-gp/sama-gp-annonce.jar
+        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/sama-gp/sama-gp"
+        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
         ssh vagrant@192.168.56.11 << 'EOF'
             pkill -f 'sama-gp-annonce.jar' || true  # Stop existing process if running
-            nohup java -jar /opt/deployment/sama-gp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
+            nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
         EOF
     """
 }
