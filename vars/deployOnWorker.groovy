1d84eaa6a77926952c397aeb461e04c52ef7c73c
Souleygassama <souleygassama@gmail.com>
2025-03-06 21:13:09 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -1,48 +1,18 @@
 def call() {
     echo "Copying jar to workers"
-  /*  sh '''#!/bin/bash
-        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
-        
-        scp -o StrictHostKeyChecking=no target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/
-    '''*/
-    
-  /*  sh """
-        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deployment/sama-gp"
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deployment/sama-gp/sama-gp-annonce.jar
-        ssh vagrant@192.168.56.11 << 'EOF'
-            pkill -f 'sama-gp-annonce.jar' || true  # Stop existing process if running
-            nohup java -jar /opt/deployment/sama-gp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
-        EOF
-    """*/
-    
-    
-    sshagent(['vargrant-public-key']) {
+ sshagent(['vargrant-public-key']) {
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp"
         scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar
-         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 << 'EOF'
-                nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &
-            EOF
+
+        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 << 'EOF'
+            # Stop any running instance of the application
+            pkill -f 'sama-gp-annonce.jar' || true
+            
+            # Start the application in the background
+            nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &
+        EOF
     """
 }
-    /*
-       sshagent(['vargrant-public-key']) {
-        sh """
-            ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 << 'EOF'
-                mkdir -p /home/vagrant/deploy/sama-gp
-                 chown -R vagrant:vagrant /home/vagrant/deploy/sama-gp
-                pkill -f 'samagp-api-ms.jar' || true
-            EOF
-
-        """
-    }*/
-    /*sh """
-        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
-        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/sama-gp-annonce.jar
-        ssh vagrant@192.168.56.11 << 'EOF'
-            pkill -f 'sama-gp-annonce.jar' || true  # Stop existing process if running
-            nohup java -jar /home/vagrant/deploy/samagp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
-        EOF
-    """*/
        
 }
