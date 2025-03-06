6d8522869bbcbfde9cd17184355a62a9c192d4bc
Souleygassama <souleygassama@gmail.com>
2025-03-06 12:50:13 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -1,12 +1,10 @@
 def call() {
     echo "Copying jar to workers"
-    sh '''#!/bin/bash
-        # Create the directory on the remote server
+  /*  sh '''#!/bin/bash
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
         
-        # Copy the JAR file to the remote server
         scp -o StrictHostKeyChecking=no target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/
-    '''
+    '''*/
     /*
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deployment/sama-gp"
@@ -17,4 +15,14 @@ def call() {
         EOF
     """
     */
+     
+    sh """
+        ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
+        scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/sama-gp-annonce.jar
+        ssh vagrant@192.168.56.11 << 'EOF'
+            pkill -f 'sama-gp-annonce.jar' || true  # Stop existing process if running
+            nohup java -jar /home/vagrant/deploy/samagp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
+        EOF
+    """
+    
 }
