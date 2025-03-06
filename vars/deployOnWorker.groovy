35adf0c5b60136b21095da7670970d0209f0c470
Souleygassama <souleygassama@gmail.com>
2025-03-06 19:02:07 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -15,11 +15,12 @@ def call() {
         EOF
     """
     */
+    /*
     sshagent(['vargrant-public-key']) {
     sh '''#!/bin/bash
     ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp3"
     '''
-}
+}*/
      
     /*sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
@@ -29,5 +30,22 @@ def call() {
             nohup java -jar /home/vagrant/deploy/samagp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
         EOF
     """*/
+     def jarPath = "target/samagp-api-ms-0.0.1-SNAPSHOT.jar"
+    def remoteUser = "vagrant"
+    def remoteHost = "192.168.56.11"
+    def remotePath = "/opt/deployment/sama-gp/sama-gp-annonce.jar"
+    def privateKeyPath = "~/.ssh/id_rsa"
     
+   def command = "scp -i ${privateKeyPath} -o StrictHostKeyChecking=no ${jarPath} ${remoteUser}@${remoteHost}:${remotePath}"
+    
+    println "Executing: ${command}"
+    
+    def process = command.execute()
+    process.waitFor()
+
+    if (process.exitValue() == 0) {
+        println "JAR copied successfully to ${remoteHost}:${remotePath}"
+    } else {
+        println "Failed to copy JAR. Error: ${process.err.text}"
+    }    
 }
