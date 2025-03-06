94c1d0617d60ac8e20917993fa49ce759f2d29f1
Souleygassama <souleygassama@gmail.com>
2025-03-06 20:31:29 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -15,14 +15,27 @@ def call() {
         EOF
     """*/
     
-    
+    /*
     sshagent(['vargrant-public-key']) {
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp8"
         scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp8/
     """
-}
-     
+}*/
+       sshagent(['vargrant-public-key']) {
+        sh """
+            ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "
+                mkdir -p /home/vagrant/deploy/sama-gp &&
+                sudo chown -R vagrant:vagrant /home/vagrant/deploy/sama-gp &&
+                pkill -f 'samagp-api-ms.jar' || true
+            "
+
+            scp -o StrictHostKeyChecking=no target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/samagp-api-ms.jar
+
+            ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "
+                nohup java -jar /home/vagrant/deploy/sama-gp/samagp-api-ms.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &
+            "
+        """
     /*sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deploy/samagp"
         scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/sama-gp-annonce.jar
