c635ef61f861fb5a0c6eb6a378c02e13e6db0f1a
Souleygassama <souleygassama@gmail.com>
2025-03-06 22:13:48 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -12,7 +12,6 @@ def call() {
         sh """
             ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "mkdir -p /home/vagrant/deploy/sama-gp"
             scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/sama-gp/sama-gp-annonce.jar 
-            ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo pkill -f sama-gp-annonce || true"
             ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "nohup java -jar /home/vagrant/deploy/sama-gp/sama-gp-annonce.jar > /home/vagrant/deploy/sama-gp/app.log 2>&1 &"
         """
     }
