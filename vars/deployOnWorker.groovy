b8a4f46ad77c465f69119f53ae3703bbf139fd86
Souleygassama <souleygassama@gmail.com>
2025-03-06 19:39:10 +0100

    Update deployOnWorker.groovy

--- vars/deployOnWorker.groovy
+++ vars/deployOnWorker.groovy
@@ -5,7 +5,7 @@ def call() {
         
         scp -o StrictHostKeyChecking=no target/samagp-api-ms-0.0.1-SNAPSHOT.jar vagrant@192.168.56.11:/home/vagrant/deploy/samagp/
     '''*/
-    /*
+    
     sh """
         ssh -o StrictHostKeyChecking=no vagrant@192.168.56.11 "sudo mkdir -p /home/vagrant/deployment/sama-gp"
         scp -o StrictHostKeyChecking=no target/*.jar vagrant@192.168.56.11:/home/vagrant/deployment/sama-gp/sama-gp-annonce.jar
@@ -14,7 +14,7 @@ def call() {
             nohup java -jar /opt/deployment/sama-gp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
         EOF
     """
-    */
+    
     /*
     sshagent(['vargrant-public-key']) {
     sh '''#!/bin/bash
@@ -30,22 +30,5 @@ def call() {
             nohup java -jar /home/vagrant/deploy/samagp/sama-gp-annonce.jar > /opt/deployment/app.log 2>&1 &
         EOF
     """*/
-     def jarPath = "target/samagp-api-ms-0.0.1-SNAPSHOT.jar"
-    def remoteUser = "vagrant"
-    def remoteHost = "192.168.56.11"
-    def remotePath = "/opt/deployment/sama-gp/sama-gp-annonce.jar"
-    def privateKeyPath = "~/.ssh/id_rsa"
-    
-   def command = "scp -i ${privateKeyPath} -o StrictHostKeyChecking=no ${jarPath} ${remoteUser}@${remoteHost}:${remotePath}"
-    
-    println "Executing: ${command}"
-    
-    def process = command.execute()
-    process.waitFor()
-
-    if (process.exitValue() == 0) {
-        println "JAR copied successfully to ${remoteHost}:${remotePath}"
-    } else {
-        println "Failed to copy JAR. Error: ${process.err.text}"
-    }    
+       
 }
