1679a4c4ac9e861fecb8a3b64fe72bfcaacfcf5e
Souleygassama <souleygassama@gmail.com>
2025-03-16 15:28:18 +0100

    Update EnvVariablesLoader.groovy

--- vars/EnvVariablesLoader.groovy
+++ vars/EnvVariablesLoader.groovy
@@ -1,1 +1,20 @@
+import java.nio.file.Files
+import java.nio.file.Paths
 
+def call(String filePath = "../.env") {
+    def envVars = [:]
+    
+    if (Files.exists(Paths.get(filePath))) {
+        def lines = Files.readAllLines(Paths.get(filePath))
+        lines.each { line ->
+            if (line.trim() && !line.startsWith("#")) { // Ignore comments and empty lines
+                def (key, value) = line.split("=", 2)
+                envVars[key.trim()] = value.trim()
+            }
+        }
+    } else {
+        error "ERROR: .env file not found at ${filePath}"
+    }
+
+    return envVars
+}
